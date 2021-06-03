9bdeb2a5a0087e8e5de90c4de7f70c995a4f02ab
Max Rydahl Andersen <manderse@redhat.com>
2021-06-03 02:19:32 +0200

    disable checkstyle as impossible to manually fix

--- src/test/java/com/ongres/process/SimpleFluentProcessIT.java
+++ src/test/java/com/ongres/process/SimpleFluentProcessIT.java
@@ -26,30 +26,31 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-import static com.ongres.process.FluentProcess.*;
+import static com.ongres.process.FluentProcess.$;
 
 public class SimpleFluentProcessIT {
 
-    @Test
-    public void testShell() throws Exception {
-        Assertions.assertIterableEquals(
+  @Test
+  public void testShell() throws Exception {
+    Assertions.assertIterableEquals(
                 Arrays.asList("hello", "world"),
                 $("echo hello; echo world")
                         .stream().collect(Collectors.toList()));
-    }
+  }
 
-    @Test
-    public void testShellPipe() throws Exception {
+  @Test
+  public void testShellPipe() throws Exception {
         Assertions.assertIterableEquals(
                 Arrays.asList("hello", "world"),
                 $("echo hello; echo world")
                         .pipe$("cat")
                         .stream().collect(Collectors.toList()));
-    }
+  }
 
-    @Test
-    public void testString() {
+  @Test
+  public void testString() {
 
-        Assertions.assertEquals($("echo hello| wc -c").get().trim(),"6");
-    }
+        Assertions.assertEquals($("echo hello| wc -c").get().trim(), "6");
+  }
+
 }
